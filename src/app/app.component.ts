dfcbd8eefe07fee0fdb2974203bb6f2e47acb982
roman <root@localhost>
2017-04-24 17:14:01 +0400

    Keep app template without anything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: `
-    <div>
-        <a [routerLink]="['/']">Home</a>
-        <a [routerLink]="['welcome']">Welcome</a>
-    </div>
-    <router-outlet></router-outlet>`,
+    template: `<router-outlet></router-outlet>`,
     styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
