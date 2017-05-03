6e6a5018164ef75352850da259960969711615f6
iamthes <s91630277@gmail.com>
2017-05-03 22:44:27 +0400

    Renamed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HomeComponent } from './home/home.component';
-import { TranslationModule } from './translation.module';
+import { APP_TRANSLATION } from './app.translation';
 import { APP_PROVIDERS } from './app.providers';
 import { ConfigStaticLoader, ConfigLoader, ConfigModule } from '@ngx-config/core';
 import { APP_ROUTES } from './app.routes';
@@ -20,7 +20,7 @@ export function configFactory(): ConfigLoader {
         FormsModule,
         APP_ROUTES,
         ConfigModule.forRoot({ provide: ConfigLoader, useFactory: configFactory }),
-		TranslationModule,
+		APP_TRANSLATION,
     ],
     declarations: [
         AppComponent,
